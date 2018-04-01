749b66ee52f675fbca4da84fe5ab6a6f850b7b51
SkyVault <dustinneumann42@gmail.com>
2018-03-31 18:48:56 -0700

    Tiled maps now load

--- Coral/tiled.nim
+++ Coral/tiled.nim
@@ -4,14 +4,12 @@ import
     streams,
     strutils,
     graphics,
+    parseutils,
     os,
     game,
     ospaths
 
 type
-    TiledLayer* = ref object
-    TiledObject* = ref object
-
     TiledOrientation {.pure.} = enum
         Orthogonal,
         Orthographic
@@ -19,12 +17,19 @@ type
     TiledRenderorder {.pure.} = enum
         RightDown
 
+    TiledObject* = ref object
+
     TiledTileset* = ref object
         name: string
         tilewidth, tileheight: int
         tilecount: int
         columns: int
         image: Image
+
+    TiledLayer* = ref object
+        name: string
+        width, height: int
+        tiles: seq[int]
 
     TiledMap* = ref object
         version: string
@@ -37,6 +42,7 @@ type
         infinite: bool
 
         tilesets: seq[TiledTileset]
+        layers: seq[TiledLayer]
 
     
 proc loadTileset* (path: string): TiledTileset=
@@ -67,7 +73,8 @@ proc loadTiledMap* (path: string): TiledMap=
     assert(fileExists path, "[ERROR] :: loadTiledMap :: Cannot find map: " & path)
 
     result = TiledMap(
-        tilesets: newSeq[TiledTileset]()
+        tilesets: newSeq[TiledTileset](),
+        layers: newSeq[TiledLayer]()
     )
 
     let theXml = readFile(path)
@@ -105,3 +112,35 @@ proc loadTiledMap* (path: string): TiledMap=
     for node in tileset_xmlnodes:
         let tpath = parentDir(path) & "/" & node.attr "source"
         result.tilesets.add loadTIleset(tpath)
+    
+    let layers_xmlnodes = theXml.findAll "layer"
+    let objects_xmlnodes = theXml.findAll "objectgroup"
+
+    for layerXml in layers_xmlnodes:
+        let layer = TiledLayer(
+            name: layerXml.attr "name",
+            width: layerXml.attr("width").parseInt,
+            height: layerXml.attr("height").parseInt,
+        )
+
+        layer.tiles = newSeq[int](layer.width * layer.height)
+
+        let dataXml = layerXml[0][0]
+        let dataText = dataXml.rawText
+        let dataTextLen = dataText.len
+
+        var cursor = 0
+        var index = 0
+        var token = ""
+
+        while cursor < dataTextLen:
+            cursor += parseUntil(dataText, token, ',', cursor) + 1
+            token.removeSuffix()
+            token.removePrefix()
+            layer.tiles[index] = token.parseInt
+            index += 1
+
+        result.layers.add(layer)
+
+    for objectsXml in objects_xmlnodes:
+        discard """ TODO: Implement"""
