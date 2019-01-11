8ada33a94ed7b7a63df0981fbcc5a40fff682769
neumdu <dustinneumann42@gmail.com>
2019-01-10 23:04:49 -0800

    Started rotation with line rectangles

--- tests/platformer.nim
+++ tests/platformer.nim
@@ -51,7 +51,7 @@ proc draw(game: var Game)=
       drawRect(x * TileSize, y * TileSize, TileSize, TileSize)
 
   setDrawColor Green
-  drawRect(100, 100, 200, 100, Time.timer, 100, 50)
+  drawLineRect(100, 100, 200, 100, Time.timer, 100, 50)
 
 initGame(
   1280,
